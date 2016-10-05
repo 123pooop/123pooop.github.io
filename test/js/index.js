72733ec37f1107c4a6a9b879f72e12d9df36a608
123pooop <18chutj2@webmail.sis.edu.hk>
2016-10-05 09:55:46 +0800

    Test

---
+++ test/js/index.js
@@ -0,0 +1,87 @@
+function test() {
+  
+  var one = prompt ("What is 1+1");
+  
+  if(one === "2") {
+   alert("Correct")
+ }
+  
+  else {
+    alert("Wrong");
+  }
+
+var two = prompt ("Where did i get my background from?")
+
+if(two === "google"){
+alert("Correct")
+}  
+
+  else{
+    alert("Nice try");
+  }
+
+
+var three = prompt("Is seth bad at CS:GO")
+
+if(three === "yes"){
+  alert("obviously")
+}
+else{
+  alert("wrong. why would he be good");
+}
+
+var four = prompt("Is loki good at CS:GO")
+
+if(four === "no"){
+  alert("true")
+}
+  else{
+    alert("I am better than him");
+  }
+
+var five = prompt("Am I the best awper ever")
+
+if(five === "yes"){
+  alert("true")
+}
+  
+  else{
+    alert("if I wasnt the best awper who would I be");
+  }
+}
+
+if(one === "2"){
+  score +1
+}
+else{
+  score -1
+};
+   
+if(two ==="google"){
+  score +1
+}
+else{
+  score -1
+};
+
+if(three === "yes"){
+  score +1}
+  
+else{
+  score-1
+};
+
+if(four === "no"){
+  score +1}
+  
+else{
+  score -1
+};
+
+if(five === "yes"){
+  score +1
+}
+
+else {
+  score -1
+};
